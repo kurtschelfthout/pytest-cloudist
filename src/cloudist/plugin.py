45edb58a8fa25983e9b50a0662594bb9b6e1c223
Kurt Schelfthout <kurt.schelfthout@gmail.com>
2022-08-03 16:21:25 +0100

    Prepend cd to all options.

--- src/cloudist/plugin.py
+++ src/cloudist/plugin.py
@@ -20,7 +20,7 @@ def pytest_addoption(parser):
         ),
     )
     group.addoption(
-        "--num-workers",
+        "--cd-num-workers",
         dest="num_workers",
         metavar="num_workers",
         action="store",
@@ -29,7 +29,7 @@ def pytest_addoption(parser):
         help="Number of workers to use for distributed testing.",
     )
     group.addoption(
-        "--cpu",
+        "--cd-cpu-per-worker",
         dest="logical_cpu_per_worker",
         metavar="logical_cpu_per_worker",
         action="store",
@@ -38,7 +38,7 @@ def pytest_addoption(parser):
         help="The number of logical CPUs needed per worker.",
     )
     group.addoption(
-        "--memory",
+        "--cd-memory-per-worker",
         dest="memory_gb_per_worker",
         metavar="memory_gb_per_worker",
         action="store",
@@ -47,14 +47,14 @@ def pytest_addoption(parser):
         help="The amount of memory (in GiB) needed per worker.",
     )
     group.addoption(
-        "--interrupt-prob",
+        "--cd-interrupt-prob",
         dest="interruption_probability_threshold",
         action="store",
         default=80,
-        help="The estimated probability that spot instance will get interrupted by AWS. Set to 0 for on-demand instances, which will be up to 10x more expensive.",
+        help="The estimated probability that spot instances are interrupted by AWS. Set to 0 for on-demand instances, which will be up to 10x more expensive.",
     )
     group.addoption(
-        "--extra-files",
+        "--cd-extra-files",
         dest="extra_files",
         action="append",
         help="Extra files to copy as to the remote machines, if needed. .py files on sys.path are copied automatically.",
